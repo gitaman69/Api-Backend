c20a28a41995f418d25e1a0a6188de12ca02f363
gitaman69 <166131965+gitaman69@users.noreply.github.com>
2025-09-12 02:10:16 +0530

    firebase

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,9 +1,8 @@
-// firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./electricq-d30b7-firebase-adminsdk-fbsvc-a48728e7cd.json");
 
-// Initialize Firebase Admin only once
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_JSON);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
