a3e23ed8352d82a3f6891f85681f4d2435eab30f
gitaman69 <166131965+gitaman69@users.noreply.github.com>
2025-09-12 03:40:36 +0530

    schema

--- models/User.js
+++ models/User.js
@@ -1,15 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, required: true, unique: true },
   otp: String,
   otpExpiresAt: Date,
-  // Add an array to store multiple Expo push tokens (for multi-device)
-  expoPushTokens: {
-    type: [String],
-    default: [],
-  },
+  expoPushTokens: [String],
+  fcmTokens: [String],
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
